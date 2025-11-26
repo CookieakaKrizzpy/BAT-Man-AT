dbcbada80d576078bb16a0cb4daaf9c327976648
CookieakaKrizzpy <michael.seuberth@gmail.com>
2025-11-26 10:35:22 +0100

    Fix PDO connection setup in login.php by correcting variable names and syntax errors

--- auth/login.php
+++ auth/login.php
@@ -10,21 +10,21 @@ header ("Access-Control_Allow-Origin: *");                                      
 $db_host = "localhost";                                                                    // Datenbank Host
 $db_name = "TEST";                                                                         // Datenbank Name
 $db_user = "TEST";                                                                         // Datenbank Benutzer
-$db_pass = "";                                                                             // Datenbank Passwort
+$db_password = "";                                                                         // Datenbank Passwort
 
 // Verbindung aufbauen (PDO)
 
 try
 {
-    $dsn = mysql:host=$db_host;
-    $db_name = "dbname=$db_name;charset=utf8mb4";
-}
-    §$options = [
+    $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";                          // Data Source Name sagt PDO welcher Treiber und welche DB genutzt werden soll
+
+    $options = [
         PDO:ATTR_ERRMODE => PDO:ERRMODE_EXCEPTION,
         PDO:ATTR_DEFAULT_FETCH_MODE => PDO:FETCH_ASSOC,
         PDO:ATTR_EMULATE_PREPARES => false,
     ];
-
+    $pdo = new PDO($dsn, $db_user, $db_password, $options);                                // Neue PDO Instanz erstellen
+}
 // Anfragemethode prüfen 
 
 // Eingabedaten prüfen/bereinigen
