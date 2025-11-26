7c74480722696855037a835e3c9b46154746e75d
CookieakaKrizzpy <michael.seuberth@gmail.com>
2025-11-26 09:29:55 +0100

    Add initial implementation of login.php with headers and database connection setup

---
+++ auth/login.php
@@ -0,0 +1,24 @@
+<?php
+
+// Grundeinstellungen, Header?
+
+header ("Content-Type: application/json; charset=UTF-8");                               // JSON HEADER
+header ("Access-Control_Allow-Origin: *");                                              // CORS HEADER (falls Dashboard extern gehostet wird)
+
+// Datenbankverbindung herstellen
+
+$db_host = "localhost";                                                                    // Datenbank Host
+$db_name = "TEST";                                                                         // Datenbank Name
+$db_user = "TEST";                                                                         // Datenbank Benutzer
+$db_pass = "";                                                                             // Datenbank Passwort
+
+// Verbindung aufbauen (PDO)
+
+// Anfragemethode prüfen 
+
+// Eingabedaten prüfen/bereinigen
+
+// User suchen 
+
+// Passwort prüfen
+
