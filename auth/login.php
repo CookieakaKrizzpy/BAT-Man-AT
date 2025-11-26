53e69eef1d7458334817eba4b3f598bb33fd0264
CookieakaKrizzpy <michael.seuberth@gmail.com>
2025-11-26 09:30:50 +0100

    Fix formatting of headers in login.php for consistency

--- auth/login.php
+++ auth/login.php
@@ -2,8 +2,8 @@
 
 // Grundeinstellungen, Header?
 
-header ("Content-Type: application/json; charset=UTF-8");                               // JSON HEADER
-header ("Access-Control_Allow-Origin: *");                                              // CORS HEADER (falls Dashboard extern gehostet wird)
+header ("Content-Type: application/json; charset=UTF-8");                                  // JSON HEADER
+header ("Access-Control_Allow-Origin: *");                                                 // CORS HEADER (falls Dashboard extern gehostet wird)
 
 // Datenbankverbindung herstellen
 
