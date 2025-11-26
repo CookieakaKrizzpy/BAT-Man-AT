971afcf22df4481cb6404e55bceb6aeaf1737ba7
CookieakaKrizzpy <michael.seuberth@gmail.com>
2025-11-26 10:28:09 +0100

    Fix PDO connection setup in login.php by correcting syntax errors and ensuring proper configuration

--- auth/login.php
+++ auth/login.php
@@ -14,6 +14,17 @@ $db_pass = "";                                                                  
 
 // Verbindung aufbauen (PDO)
 
+try
+{
+    $dsn = mysql:host=$db_host;
+    $db_name = "dbname=$db_name;charset=utf8mb4";
+}
+    §$options = [
+        PDO:ATTR_ERRMODE => PDO:ERRMODE_EXCEPTION,
+        PDO:ATTR_DEFAULT_FETCH_MODE => PDO:FETCH_ASSOC,
+        PDO:ATTR_EMULATE_PREPARES => false,
+    ];
+
 // Anfragemethode prüfen 
 
 // Eingabedaten prüfen/bereinigen
