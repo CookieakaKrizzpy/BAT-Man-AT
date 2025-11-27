786575eee01bcf93eb552ae8927719368ba58a3e
DocKoRn <doc-korn@hotmail.de>
2025-11-27 13:41:45 +0100

    Fix PDO fetch mode syntax in login.php

--- restore/login.php
+++ restore/login.php
@@ -17,7 +17,7 @@ try
 
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO:FETCH_ASSOC,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     ];
     $pdo = new PDO($dsn, $db_user, $db_password, $options);                                // Neue PDO Instanz erstellen
