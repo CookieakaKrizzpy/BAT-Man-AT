f67247867b29ae21887f4a99fba4436d37c213bd
CookieakaKrizzpy <michael.seuberth@gmail.com>
2025-11-28 10:42:57 +0100

    feat: Implement magic link authentication for instructors including token generation, email sending, and verification.

--- auth/login.php
+++ auth/login.php
@@ -1,40 +1,41 @@
 <?php
 
-header ("Content-Type: application/json; charset=UTF-8");                                  // JSON HEADER
-header ("Access-Control-Allow-Origin: *");                                                 // CORS HEADER (falls Dashboard extern ge
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
 
 // Datenbankverbindung herstellen
-$db_host = "localhost";                                                                    // Datenbank Host
-$db_name = "it202407";                                                                     // Datenbank Name
-$db_user = "batman";                                                                       // Datenbank Benutzer
-$db_password = "batman";                                                                   // Datenbank Passwort
+$db_host = "192.168.9.123";                                                                   // Adresse ändern!
+$db_name = "it202407";
+$db_user = "batman";
+$db_password = "batman";
 
 // Verbindung aufbauen (PDO)
 try
 {
-    $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";                          // Data Source Name sagt PDO welcher Treiber und welche DB genutzt werden soll
-
+    $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
+    
     $options = [
-        PDO::ATTR_ERRMODE => PDO:ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO:FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     ];
-    $pdo = new PDO($dsn, $db_user, $db_password, $options);                                // Neue PDO Instanz erstellen
+    $pdo = new PDO($dsn, $db_user, $db_password, $options);
 }
 catch (PDOException $e)
 {
     echo json_encode(
-        ["status" => "error", "message" => "Verbindungsfehler: " . $e->getMessage()]);     // Fehlermeldung als JSON ausgeben
+        ["status" => "error", "message" => "Verbindungsfehler: " . $e->getMessage()]);
+    exit();
 }
 
 // Anfragemethode prüfen 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {                                              // Nur POST Anfragen erlauben
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     echo json_encode(
         ["status" => "error", "message" => "Nur POST Anfragen erlaubt"]);
     exit();
 }
 
-$login_type = isset($_POST["login_type"]) ? $_POST["login_type"] : "Teilnehmer"
+$login_type = isset($_POST["login_type"]) ? $_POST["login_type"] : "Teilnehmer";
 
 if ($login_type == "Ausbilder") {
     require "auth_ausbilder.php";
